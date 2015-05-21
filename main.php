7147d6e10710c74e11e5bb71d687426ecab2e620
Joeunha <imjoeunha@gmail.com>
2015-05-21 11:42:34 +0900

    search api 적용 완료!

--- main.php
+++ main.php
@@ -25,7 +25,7 @@
     </div><!-- /header -->
     
     <div class="page-title">
-    	<h2>NEWS FEED</h2>
+    	<h2 class="page-title">NEWS FEED</h2>
     </div>
 
 <?php
@@ -39,47 +39,29 @@ $access_token = $_SESSION['access_token'];
 
 $connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token['oauth_token'], $access_token['oauth_token_secret']);
 
-$statuses = $connection->get("search/tweets", array("q" => "#taleline", "since_id" => 12345, "count" => 10));
+$statuses = $connection->get("search/tweets", array("q" => "#taleline"));
 
-$news_feed = array(); //2d array를 선언해서 아래의 $index 값들을 삽입해준다. 이때 연관배열을 활용하면 쉬울 것!!
-
-foreach ($statuses as $element){
-    foreach($element as $index){
-        = $index->user->name;
-        = $index->user->profile_image_url;
-        = $index->text;
-    }
+foreach ($statuses->statuses as $element){
+    $user_name = $element->user->name;
+    $profile_image = $element->user->profile_image_url;
+    $tweet_text = $element->text;
+    
+    echo "<ul>";
+    echo "<div class=\"container\" data-role=\"content\">";
+    echo "<div class=\"profile\">";
+    echo "<img class=\"thumnail\" src=\"".$profile_image."\" alt=\"\" />";
+    echo "<a herf=\"http://twitter.com/$user_name\" class=\"name\">@$user_name</a>";
+//    echo "<h5 class=\"slogan\">This is slogan</h5>";
+    echo "</div>";
+    echo "<div>";
+    echo "<p class=\"news\">$tweet_text</p>";
+    echo "</div>";
+    echo "</div>";
+    echo "</ul>";   
 }
 
 ?>
-
-<!-- <div class="container" data-role="content">
-        <div class="profile">
-            <img class="thumbnail" src="/img/pebble_logo.png" alt="" />
-            <h4 class="name">pebble</h4> 
-            <h5 class="slogan">Awesome watch. No compromises.</h4>
-        </div>
-      <p class="news">1941 KONRAD ZUSE INVENTS HOME COMPUTING. <br> 2009 ERIC MIGICOVSKY TOTALLY NAILS WEARABLE COMPUTING. <br> 2011 AFTER MUCH DELIBERATION PEBBLE IS BORN.</p>
-    </div>/content
-    
-    <div class="container" data-role="content">
-        <div class="profile">
-            <img class="thumbnail" src="/img/hisbeans_logo.png" alt="" />
-            <h4 class="name">hisbeans</h4> 
-            <h5 class="slogan">희망을 주는 하나님의 기업</h4>
-        </div>
-      <p class="news">2008 향기내는사람들 창립 <br> 2009 HISBEANS 카페 1호점 한동대점 오픈 <br> 2011 HISBEANS 카페 2호점 포항시립중앙아트홀점 오픈 </p>
-    </div>/content
-    
-    <div class="container" data-role="content">
-        <div class="profile">
-            <img class="thumbnail" src="/img/heeum_logo.jpeg" alt="" />
-            <h4 class="name">Heeum</h4> 
-            <h5 class="slogan">BLOOMING THEIR HOPES WITH YOU</h4>
-        </div>
-      <p class="news">2012 (사)정신대할머니와 함께하는 시민모임과 고려대 Enacts Blooming Project 희움 런칭 <br> 2013 환경재단 '2013 세상을 밝게 만든 사람들' 수상</p>
-    </div>/content -->
-
+   
     <div data-role="footer" data-id="foo1" data-position="fixed" data-fullscreen="true">
         <div data-role="navbar">
             <ul>
